c194f6b9b428a10542036f2701a4442ef320b341
Kelp <kelp@phate.org>
2012-05-09 21:56:53 +0800

    add ARC version

---
+++ ValueInjector.h
@@ -0,0 +1,31 @@
+//
+//  ValueInjector   1.0.1
+//
+//  Created by Kelp on 12/5/6.
+//  Copyright (c) 2012年 Kelp http://kelp.phate.org/
+//  MIT License
+//
+//  1.0.1
+//      add ARC version
+//
+//  1.0
+//      Inject value from NSDictionary to custom class
+//      NSObject (ValueInjector)
+//      - (id)injectFromObject:(NSObject *)object;
+//
+//      init NSDictionary with custom class
+//      NSDictionary (ValueInjector)
+//      - (id)initWithObject:(NSObject *)object;
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+
+@interface NSObject (ValueInjector)
+- (id)injectFromObject:(NSObject *)object;
+@end
+
+@interface NSDictionary (ValueInjector)
+- (id)initWithObject:(NSObject *)object;
+@end
